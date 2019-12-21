4f1cdf7f86310395d12bc4a8c66d1b25eafdc78c
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 16:01:44 +0100

    fix model tests

--- tests/model.php
+++ tests/model.php
@@ -1,20 +1,19 @@
 <?php
 
 use Gentry\Gentry\Wrapper;
+use Codger\Lodger\Model;
 
-$recipe = include 'recipes/model/Recipe.php';
 $inout = Wrapper::createObject(Codger\Generate\FakeInOut::class);
 Codger\Generate\Recipe::setInOut($inout);
 putenv("CODGER_DRY=1");
 
 /** Test model recipe */
-return function () use ($recipe, $inout) : Generator {
-    $bootstrap = new Codger\Generate\Bootstrap('model');
+return function () : Generator {
     /** The recipe should make us a basic model according to our parameters */
-    yield function () use ($recipe, $inout, $bootstrap) {
-        $bootstrap->resetOptions();
-        $bootstrap->setOptions(['^prefill', '^ornament']);
-        $result = $recipe->call($bootstrap, 'User')->render();
+    yield function () {
+        $model = new Model(['User', '--skip-prefill']);
+        $model->execute();
+        $result = $model->render();
         assert(strpos($result, <<<EOT
 namespace User;
 
@@ -25,14 +24,14 @@ EOT
     };
     
     /** The recipe should make us a model using a db table as a base */
-    yield function () use ($recipe, $inout, $bootstrap) {
+    yield function () {
         $db = new PDO('pgsql:dbname=codger_test', 'codger_test', 'blarps');
         $db->exec(file_get_contents(dirname(__DIR__).'/info/fixture.sql'));
-        $bootstrap->resetOptions();
-        $bootstrap->setOptions(['prefill', '^ornament']);
-        $result = $recipe->call($bootstrap, 'Users', 'users', 'pgsql', 'codger_test', 'blarps')->render();
+        $model = new Model(['User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps']);
+        $model->execute();
+        $result = $model->render();
         assert(strpos($result, <<<EOT
-namespace Users;
+namespace User;
 
 class Model
 {
@@ -49,30 +48,14 @@ EOT
     };
     
     /** It can also add Ornament to our model */
-    yield function () use ($recipe, $inout, $bootstrap) {
-        $bootstrap->resetOptions();
+    yield function () {
         $db = new PDO('pgsql:dbname=codger_test', 'codger_test', 'blarps');
         $db->exec(file_get_contents(dirname(__DIR__).'/info/fixture.sql'));
-        $result = $recipe->call($bootstrap, 'Users', 'users', 'pgsql', 'codger_test', 'blarps')->render();
-        assert(strpos($result, <<<EOT
-namespace Users;
-
-use Ornament\Core;
-
-class Model
-{
-    use Core\Model;
-
-    public \$id;
-    /** @var string */
-    public \$username;
-    /** @var string */
-    public \$password;
-    /** @var string */
-    public \$description;
-}
-EOT
-        ) !== false);
+        $model = new Model(['User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps', '--ornament']);
+        $model->execute();
+        $result = $model->render();
+        assert(strpos($result, 'use Ornament\Core;') !== false);
+        assert(strpos($result, 'use Core\Model;') !== false);
     };
 };
 
