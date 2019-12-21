53e1c5f38f570f942d2f59d08362d79c17242209
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 16:12:00 +0100

    start modernizing this

---
+++ recipes/ListView.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Codger\Lodger;
+
+use Codger\Generate\Language;
+
+class ListView extends View
+{
+    public function __invoke(string $namespace) : void
+    {
+        $this->set('name', Language::convert("$namespace\\ListView", Language::TYPE_PHP_NAMESPACE));
+        $repo = Language::convert($namespace, Language::TYPE_VARIABLE);
+        $things = Language::pluralize($repo);
+        if (!isset($this->template)) {
+            $this->template = Language::convert($namespace, Language::TYPE_PATH).'/template.html.twig';
+        }
+        $this->defineProperty($things, null, 'public', '@var array')
+            ->defineProperty('template', $template, 'protected', '@var string')
+            ->defineProperty("{$repo}Repository", null, 'protected', "@var $namespace\\Repository")
+            ->addMethod('__construct', function () use ($things, $repo) : string {
+                return <<<EOT
+    parent::__construct();
+    \$this->inject(function (\${$repo}Repository) {});
+    \$this->{$things} = \$this->{$repo}Repository->all();
+EOT;
+            })
+            ->output(getcwd().'/src/'.Language::convert($namespace, Language::TYPE_PATH).'/View.php');
+        $this->delegate('lodger:list:template', "--template={$this->template}", "--repository=$repo");
+    }
+}
+
