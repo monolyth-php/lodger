100a4ffadcbce9a2e310291de9a493e197ef6f9c
Marijn Ophorst <marijn@sensimedia.nl>
2023-07-05 15:35:32 +0200

    wrap, and test for databaserepository extension instead

--- tests/repository.php
+++ tests/repository.php
@@ -7,74 +7,43 @@ Codger\Generate\Recipe::setInOut($inout);
 
 /** Test repository recipe */
 return function () : Generator {
-    /** Create a Foo repository with all expected operations */
-    yield function () {
-        $recipe = new Codger\Lodger\Repository(['Foo', '--table=foo']);
-        $recipe->execute();
-        $result = $recipe->render();
+    $recipe = new Wrapper(new Codger\Lodger\Repository(['Foo', '--table=foo']));
+    $recipe->execute();
+    $result = $recipe->render();
+
+    /** We are in the correct namespace */
+    yield function () use ($result) {
         assert(strpos($result, 'namespace Foo;') !== false);
-        
-        /** Find method */
+    };
+
+    /** We have the correct general class definition */
+    yield function () use ($result) {
+        assert(strpos($result, <<<EOT
+use Sensimedia\Supportery\DatabaseRepository;
+
+class Repository extends DatabaseRepository
+{
+EOT
+        ) !== false);
+    };
+
+    /** We get an `all` method shorthand */
+    yield function () use ($result) {
+        assert(strpos($result, <<<EOT
+    public function all() : array
+    {
+        return \$this->list(\$this->select());
+    }
+EOT
+        ) !== false);
+    };
+
+    /** We get a `find` method shorthand */
+    yield function () use ($result) {
         assert(strpos($result, <<<EOT
     public function find(int \$id) :? Model
     {
-        try {
-            return \$this->adapter->selectFrom('foo')
-                ->where('id = ?', \$id)
-                ->fetchObject(Model::class);
-        } catch (SelectException \$e) {
-            return null;
-        }
-    }
-EOT
-        ) !== false);
-        
-        /** Save method */
-        assert(strpos($result, <<<EOT
-    public function save(Model &\$model) :? string
-    {
-        \$data = [];
-        \$reflection = new ReflectionObject(\$model);
-        foreach (\$reflection->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED & ~ReflectionProperty::IS_STATIC) as \$property) {
-            \$property->setAccessible(true);
-            if (\$property->name != 'id' && (isset(\$model->id) || !is_null(\$property->getValue(\$model)))) {
-                \$data[\$property->name] = \$property->getValue(\$model);
-            }
-        }
-        try {
-            if (isset(\$model->id)) {
-                \$this->adapter->updateTable('foo')
-                    ->where('id = ?', \$model->id)
-                    ->execute(\$data);
-                \$model = \$this->find(\$model->id);
-            } else {
-                \$this->adapter->insertInto('foo')
-                    ->execute(\$data);
-                \$model = \$this->find(\$this->adapter->lastInsertId('foo'));
-            }
-            return null;
-        } catch (InsertException \$e) {
-            return 'insert';
-        } catch (UpdateException \$e) {
-            return 'update';
-        }
-    }
-EOT
-        ) !== false);
-
-        /** Delete method */
-        assert(strpos($result, <<<EOT
-    public function delete(Model &\$model) :? string
-    {
-        try {
-            \$this->adapter->deleteFrom('foo')
-                ->where('id = ?', \$model->id)
-                ->execute();
-            \$model = null;
-            return null;
-        } catch (DeleteException \$e) {
-            return 'database';
-        }
+        return \$this->findByIdentifier(\$id);
     }
 EOT
         ) !== false);
