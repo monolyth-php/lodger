aaf636ffa6e33675c550944c208a750a10883a7a
Marijn Ophorst <marijn@sensimedia.nl>
2023-07-05 16:17:30 +0200

    set composer-dir, and reset composer.json in tmp dir

--- tests/model.php
+++ tests/model.php
@@ -2,12 +2,17 @@
 
 use Gentry\Gentry\Wrapper;
 use Codger\Lodger\Model;
+use Codger\Php\Composer;
 
 $inout = new Codger\Generate\FakeInOut;
 Codger\Generate\Recipe::setInOut($inout);
 
 /** Test model recipe */
 return function () : Generator {
+    $this->beforeEach(function () {
+        copy('info/composer.json', 'tmp/composer.json');
+    });
+
     /** The recipe should make us a basic model according to our parameters */
     yield function () {
         $model = new Wrapper(new Model(['User', '--skip-prefill']));
@@ -27,7 +32,9 @@ EOT
     yield function () {
         $db = new PDO('pgsql:dbname=codger_test', 'codger_test', 'blarps');
         $db->exec(file_get_contents(dirname(__DIR__).'/info/fixture.sql'));
-        $model = new Wrapper(new Model(['User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps']));
+        $model = new Wrapper(new Model(
+            ['User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps']
+        ));
         $model->execute();
         $result = $model->render();
         assert(strpos($result, <<<EOT
@@ -55,8 +62,10 @@ EOT
     yield function () {
         $db = new PDO('pgsql:dbname=codger_test', 'codger_test', 'blarps');
         $db->exec(file_get_contents(dirname(__DIR__).'/info/fixture.sql'));
+        $composer = new Composer('tmp');
+        assert($composer->hasDependency('ornament/core') === false);
         $model = new Wrapper(new Model([
-            'User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps', '--ornament',
+            'User', '--vendor=pgsql', '--table=users', '--database=codger_test', '--user=codger_test', '--pass=blarps', '--ornament', '--composer-dir=tmp',
         ]));
         $model->execute();
         $result = $model->render();
